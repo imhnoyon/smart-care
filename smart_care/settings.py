264099d1f28c3579e80b480bf37eaa533279cce2
ihmnoyon <noyonhasan320@gmail.com>
2024-07-27 14:28:34 +0600

    update

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)&%#=b$_dg_gwfe7snkcsh3%f5j-og3$sjukuikiv=r*%!tm+_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
